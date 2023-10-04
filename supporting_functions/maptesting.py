ec1fd1a508bef7a51e1e8a9e4941f5f2f0d03407
john <ypoh007@e.ntu.edu.sg>
2023-10-04 20:03:32 +0800

    added some comments

--- supporting_functions/maptesting.py
+++ supporting_functions/maptesting.py
@@ -5,11 +5,19 @@ import random
 from agents.navigation.basic_agent import BasicAgent
 import time
 
+# sources
+# https://carla.readthedocs.io/en/0.9.14/core_map/#carla-maps
+# https://carla.readthedocs.io/en/0.9.14/tuto_M_custom_layers/
+# https://carla.readthedocs.io/en/latest/core_map/#changing-the-map
+
 # Carla world generation
 client = carla.Client('localhost', 2000)
 world = client.get_world()
 client.load_world('Town02_Opt') # Layered map https://carla.readthedocs.io/en/0.9.14/core_map/#carla-maps
 world.unload_map_layer(carla.MapLayer.All) # Causes crash when you try to re-run script with existing CARLA application
+
+# client.load_world('Town04')
+# client.load_world('Town10HD')
 
 # spawn vehicle
 vehicle_blueprints = world.get_blueprint_library().filter('vehicle.tesla.model3')
