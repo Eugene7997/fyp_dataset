123b825d4bf88483a50ae0245e5df4917b3e09d1
john <ypoh007@e.ntu.edu.sg>
2023-10-05 10:14:07 +0800

    trying to make a flying vehicle. More of a bug than feature

---
+++ make_car_fly.py
@@ -0,0 +1,87 @@
+import numpy as np
+from PIL import Image
+import carla
+import random
+from agents.navigation.basic_agent import BasicAgent
+import time
+
+def semanticSegmentationFlowCallback(image):
+    image.convert(carla.ColorConverter.CityScapesPalette)
+    buffer = np.frombuffer(image.raw_data, dtype=np.uint8)
+    buffer = buffer.reshape(image.height, image.width, 4)[..., [2, 1, 0]]  # BGRA -> RGB
+    Image.fromarray(buffer).save(f"./out/ss{image.frame}.png", "PNG")
+
+def opticalFlowCallback(data):
+    image = data.get_color_coded_flow()
+    buffer = np.frombuffer(image.raw_data, dtype=np.uint8)
+    buffer = buffer.reshape(image.height, image.width, 4)[..., [2, 1, 0]]  # BGRA -> RGB
+    Image.fromarray(buffer).save(f"./out/of{data.frame}.jpeg", "JPEG")
+
+# Carla world generation
+client = carla.Client('localhost', 2000)
+world = client.get_world()
+client.load_world('Town05')
+
+# spawn vehicle
+vehicle_blueprints = world.get_blueprint_library().filter('vehicle.tesla.model3')
+# spawn_points = world.get_map().get_spawn_points()
+custom_defined_transform = carla.Transform(carla.Location(x=31.290208, y=-11.658614, z=1.980442), carla.Rotation(yaw=-90))
+ego_vehicle = world.spawn_actor(random.choice(vehicle_blueprints), custom_defined_transform)
+
+# bring pov to vehicle
+spectator = world.get_spectator()
+spectator.set_transform(ego_vehicle.get_transform())
+
+# set destination for vehicle
+agent = BasicAgent(ego_vehicle)
+destination = carla.Location(x=32.190208, y=-65.702446, z=1.980442)
+agent.set_destination(destination)
+
+ego_vehicle.set_enable_gravity(False)
+# ego_vehicle.set_simulate_physics(False)
+
+front_left_wheel  = carla.WheelPhysicsControl(tire_friction=0)
+front_right_wheel = carla.WheelPhysicsControl(tire_friction=0)
+rear_left_wheel   = carla.WheelPhysicsControl(tire_friction=0)
+rear_right_wheel  = carla.WheelPhysicsControl(tire_friction=0)
+# front_left_wheel  = carla.WheelPhysicsControl(tire_friction=4.5, damping_rate=1.0, max_steer_angle=70.0, radius=30.0)
+# front_right_wheel = carla.WheelPhysicsControl(tire_friction=2.5, damping_rate=1.5, max_steer_angle=70.0, radius=25.0)
+# rear_left_wheel   = carla.WheelPhysicsControl(tire_friction=1.0, damping_rate=0.2, max_steer_angle=0.0,  radius=15.0)
+# rear_right_wheel  = carla.WheelPhysicsControl(tire_friction=1.5, damping_rate=1.3, max_steer_angle=0.0,  radius=20.0)
+wheels = [front_left_wheel, front_right_wheel, rear_left_wheel, rear_right_wheel]
+physics_control = ego_vehicle.get_physics_control()
+physics_control.wheels = wheels
+physics_control.mass = 0
+
+# camera
+camera_initial_transform = carla.Transform(carla.Location(z=2.5)) # Create a transform to place the camera on top of the vehicle
+IM_WIDTH = 640*2
+IM_HEIGHT = 480*2
+
+# optical flow camera 
+camera_optical_flow_blueprint = world.get_blueprint_library().find('sensor.camera.optical_flow')
+camera_optical_flow_blueprint.set_attribute('image_size_x', str(IM_WIDTH))
+camera_optical_flow_blueprint.set_attribute('image_size_y', str(IM_HEIGHT))
+camera_optical_flow = world.spawn_actor(camera_optical_flow_blueprint, camera_initial_transform, attach_to=ego_vehicle)
+spectator.set_transform(ego_vehicle.get_transform())
+camera_optical_flow.listen(lambda data: opticalFlowCallback(data))
+
+# semantic segmentation camera
+camera_semantic_segmentation_blueprint = world.get_blueprint_library().find('sensor.camera.semantic_segmentation')
+camera_semantic_segmentation_blueprint.set_attribute('image_size_x', str(IM_WIDTH))
+camera_semantic_segmentation_blueprint.set_attribute('image_size_y', str(IM_HEIGHT))
+camera_semantic_segmentation = world.spawn_actor(camera_semantic_segmentation_blueprint, camera_initial_transform, attach_to=ego_vehicle)
+spectator.set_transform(ego_vehicle.get_transform())
+# cc = carla.ColorConverter.CityScapesPalette
+# camera_semantic_segmentation.listen(lambda image: image.save_to_disk('./out/%06d.png' % image.frame, cc))
+camera_semantic_segmentation.listen(lambda data: semanticSegmentationFlowCallback(data))
+
+# move the vehicle in a straight direction
+# while True:
+#     if agent.done():
+#         print("The target has been reached, stopping the simulation")
+#         break
+#     ego_vehicle.apply_control(agent.run_step())
+
+ego_vehicle.set_autopilot(True)
+time.sleep(10)
