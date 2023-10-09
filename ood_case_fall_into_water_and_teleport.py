571fe83e50133cd5f46390ef4bd12c07039420ed
john <ypoh007@e.ntu.edu.sg>
2023-10-09 18:22:20 +0800

    ood case fall into water and teleport to simulate car movement in water

---
+++ ood_case_fall_into_water_and_teleport.py
@@ -0,0 +1,103 @@
+import numpy as np
+from PIL import Image
+import carla
+import random
+from agents.navigation.basic_agent import BasicAgent
+from queue import Queue
+from queue import Empty
+import time
+
+def semanticSegmentationFlowCallback(image, sensor_queue, sensor_name):
+    image.convert(carla.ColorConverter.CityScapesPalette)
+    buffer = np.frombuffer(image.raw_data, dtype=np.uint8)
+    buffer = buffer.reshape(image.height, image.width, 4)[..., [2, 1, 0]]  # BGRA -> RGB
+    Image.fromarray(buffer).save(f"./out/ss{image.frame}.png", "PNG")
+    sensor_queue.put((image.frame, sensor_name))
+    
+def rgbCameraCallback(image, sensor_queue, sensor_name):
+    buffer = np.frombuffer(image.raw_data, dtype=np.uint8)
+    buffer = buffer.reshape(image.height, image.width, 4)[..., [2, 1, 0]]  # BGRA -> RGB
+    Image.fromarray(buffer).save(f"./out/rgb{image.frame}.png", "PNG")
+    sensor_queue.put((image.frame, sensor_name))
+
+def opticalFlowCallback(data, sensor_queue, sensor_name):
+    image = data.get_color_coded_flow()
+    buffer = np.frombuffer(image.raw_data, dtype=np.uint8)
+    buffer = buffer.reshape(image.height, image.width, 4)[..., [2, 1, 0]]  # BGRA -> RGB
+    Image.fromarray(buffer).save(f"./out/of{data.frame}.jpeg", "JPEG")
+    sensor_queue.put((data.frame, sensor_name))
+
+# Carla world generation
+client = carla.Client('localhost', 2000)
+world = client.get_world()
+client.load_world('Town10HD_Opt')
+world.unload_map_layer(carla.MapLayer.Walls)
+
+original_settings = world.get_settings()
+settings = world.get_settings()
+settings.fixed_delta_seconds = 0.05
+settings.synchronous_mode = True
+
+sensor_queue = Queue()
+sensor_list = []
+
+# spawn vehicle
+vehicle_blueprints = world.get_blueprint_library().filter('vehicle.tesla.model3')
+spawn_points = world.get_map().get_spawn_points()
+ego_vehicle = world.spawn_actor(random.choice(vehicle_blueprints), random.choice(spawn_points))
+# camera
+camera_initial_transform = carla.Transform(carla.Location(z=1.5)) # Create a transform to place the camera on top of the vehicle
+IM_WIDTH = 640*2
+IM_HEIGHT = 480*2
+
+# semantic segmentation camera
+camera_semantic_segmentation_blueprint = world.get_blueprint_library().find('sensor.camera.semantic_segmentation')
+camera_semantic_segmentation_blueprint.set_attribute('image_size_x', str(IM_WIDTH))
+camera_semantic_segmentation_blueprint.set_attribute('image_size_y', str(IM_HEIGHT))
+camera_semantic_segmentation = world.spawn_actor(camera_semantic_segmentation_blueprint, camera_initial_transform, attach_to=ego_vehicle)
+sensor_list.append(camera_semantic_segmentation)
+
+# normal rgb camera
+camera_rgb_blueprint = world.get_blueprint_library().find('sensor.camera.rgb')
+camera_rgb_blueprint.set_attribute('image_size_x', str(IM_WIDTH))
+camera_rgb_blueprint.set_attribute('image_size_y', str(IM_HEIGHT))
+camera_rgb = world.spawn_actor(camera_rgb_blueprint, camera_initial_transform, attach_to=ego_vehicle)
+sensor_list.append(camera_rgb)
+
+# optical flow camera 
+camera_optical_flow_blueprint = world.get_blueprint_library().find('sensor.camera.optical_flow')
+camera_optical_flow_blueprint.set_attribute('image_size_x', str(IM_WIDTH))
+camera_optical_flow_blueprint.set_attribute('image_size_y', str(IM_HEIGHT))
+camera_optical_flow = world.spawn_actor(camera_optical_flow_blueprint, camera_initial_transform, attach_to=ego_vehicle)
+sensor_list.append(camera_optical_flow)
+
+# bring pov to vehicle
+spectator = world.get_spectator()
+spectator.set_transform(ego_vehicle.get_transform())
+
+camera_rgb.listen(lambda data: rgbCameraCallback(data, sensor_queue, "rgb"))
+camera_optical_flow.listen(lambda data: opticalFlowCallback(data, sensor_queue, "opticalFlow"))
+camera_semantic_segmentation.listen(lambda data: semanticSegmentationFlowCallback(data, sensor_queue, "semanticSegmentation"))
+
+
+ego_vehicle.set_autopilot(True)
+
+world.apply_settings(settings)
+
+counter = 0
+counter2 = 0;
+while True:
+    world.tick()
+    for _ in range(len(sensor_list)):
+        s_frame = sensor_queue.get(True, 1.0)
+    if (counter >= 100):
+        ego_vehicle.set_transform(carla.Transform(carla.Location(x=-227.037689+counter2, y=162.543137, z=0), carla.Rotation(pitch=1.956870, yaw=-1.042053, roll=0.000042)))
+        ego_vehicle.set_autopilot(False) # Autopilot still works but unpredictable. From visual inspection of images, no difference using autopilot and teleportation.
+        print("vehicle teleported") 
+        print(ego_vehicle.get_transform())
+        # time.sleep(1)
+        counter2 += 1
+    counter+=1
+    if (counter == 1000):
+        break
+
